a0858ff86c62fa4778f78d6f4aad0bfa30381236
fabel <fabel135@gmail.com>
2026-04-18 16:39:00 +0300

    ne dodelka

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', name='Fabel')
 @app.route('/courses')
 def courses():
     return 'это твои курсы'
+@app.route('/hello', methods=['POST'])
+def hello():
+    name = request.form['name']
+    return f'Привет, {name}!'
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    psw = request.form['psw']
+    return f'вход выполнен: {username}'
 if __name__=='__main__':
     app.run(debug=True)
